61bd8dfa27e9f837749ad05044b8de8d15cd8e9b
Ricardo Galvao <ricgalvioli@gmail.com>
2021-12-23 22:00:42 -0300

    Alteracao Multiplayer

--- multiplayers_do_base_jogo.py
+++ multiplayers_do_base_jogo.py
@@ -76,42 +76,122 @@ def multiplayers():
                 tem_alguem_jogando = False                
         
     elif multiplayers == 3:
-        print('Estamos em construção, tente outro número (tipo, 2).')
-        # tem_alguem_jogando = True
-        # while tem_alguem_jogando:
-            
-        #     nome1 = nomes_jogadores[0]
-        #     if ('id_jogador 1', nome1) in dici_vez_jogador:
-        #         print('Essa é a partida do jogador 1: Está jogando enquanto estiver o nome no dicionario') # JOGO
-        #         erro = False    
-        #         while not erro:
-                
-        #             jogador1 = game()
-        
-        #     nome2 = nomes_jogadores[1]
-        #     if ('id_jogador 2', nome2) in dici_vez_jogador:
-        #         print('Essa é a partida do jogador 1: Está jogando enquanto estiver o nome no dicionario') # JOGO
-        #         erro = False    
-        #         while not erro:
-                    
-        #             jogador2 = game()
-            
-        #     nome3 = nomes_jogadores[2]
-        #     if ('id_jogador 3', nome3) in dici_vez_jogador:
-        #         print('Essa é a partida do jogador 1: Está jogando enquanto estiver o nome no dicionario') # JOGO
-        #         erro = False    
-        #         while not erro:
-            
-        #             jogador3 = game()
-        
+         print('Estamos em construção, tente outro número (tipo, 2).')
+
+
+         nome1 = nomes_jogadores[0]
+         nome2 = nomes_jogadores[1]
+         nome3 = nomes_jogadores[2]
+
+         lista1 = ['_'] * len(dici_vez_jogador[('id_jogador 1', nome1)])
+         lista2 = ['_'] * len(dici_vez_jogador[('id_jogador 2', nome2)])
+         lista3 = ['_'] * len(dici_vez_jogador[('id_jogador 3', nome3)])
+
+         chances1 = 6
+         chances2 = 6
+         chances3 = 6
+
+         tem_alguem_jogando = True
+         while tem_alguem_jogando:
+
+             nome1 = nomes_jogadores[0]
+             if ('id_jogador 1', nome1) in dici_vez_jogador:
+                 print('Essa é a partida do jogador 1: Está jogando enquanto estiver o nome no dicionario')  # JOGO
+                 erro = False
+                 while not erro:
+                     (enforcou, acertou, chances1, erro) = game(dici_vez_jogador[('id_jogador 1', nome1)], lista1,
+                                                                chances1)
+
+                 if enforcou == True:
+                     print('E enforcou! Tente na próxima.\n\n')
+                     del dici_vez_jogador[('id_jogador 1', nome1)]
+
+                 elif acertou == True:
+                     print('Parabéns, você acertou todas as palavras!!',
+                           dici_vez_jogador[('id_jogador 1', nome1)].capitalize(), '\n\n')
+                     del dici_vez_jogador[('id_jogador 1', nome1)]
+
+             nome2 = nomes_jogadores[1]
+             if ('id_jogador 2', nome2) in dici_vez_jogador:
+                 print('Essa é a partida do jogador 2: Está jogando enquanto estiver o nome no dicionario')  # JOGO
+                 erro = False
+                 while not erro:
+                     (enforcou, acertou, chances2, erro) = game(dici_vez_jogador[('id_jogador 2', nome2)], lista2,
+                                                                chances2)
+
+                 if enforcou == True:
+                     print('E enforcou! Tente na próxima.\n\n')
+                     del dici_vez_jogador[('id_jogador 2', nome2)]
+
+                 elif acertou == True:
+                     print('Parabéns, você acertou todas as palavras!!',
+                           dici_vez_jogador[('id_jogador 2', nome2)].capitalize(), '\n\n')
+                     del dici_vez_jogador[('id_jogador 2', nome2)]
+
+             nome3 = nomes_jogadores[2]
+             if ('id_jogador 3', nome3) in dici_vez_jogador:
+                 print('Essa é a partida do jogador 3: Está jogando enquanto estiver o nome no dicionario')  # JOGO
+                 erro = False
+                 while not erro:
+                     (enforcou, acertou, chances3, erro) = game(dici_vez_jogador[('id_jogador 3', nome3)], lista3,
+                                                                chances3)
+
+                 if enforcou == True:
+                     print('E enforcou! Tente na próxima.\n\n')
+                     del dici_vez_jogador[('id_jogador 3', nome3)]
+
+                 elif acertou == True:
+                     print('Parabéns, você acertou todas as palavras!!',
+                           dici_vez_jogador[('id_jogador 3', nome3)].capitalize(), '\n\n')
+                     del dici_vez_jogador[('id_jogador 3', nome3)]
+
+             if ('id_jogador 1', nome1) not in dici_vez_jogador and (
+             'id_jogador 2', nome2) not in dici_vez_jogador and ('id_jogador 3', nome3) not in dici_vez_jogador:
+                 print('fim de jogo')
+                 tem_alguem_jogando = False
+
     else:
         print('Estamos em construção tente outro número (tipo, 2).')
-    #     # print('Jogo de 4 players!')
-    #     # print(f'Bem vindo ao jogo, {nomes_jogadores[0]}, {nomes_jogadores[1]}, {nomes_jogadores[2]} e {nomes_jogadores[3]}')
-        
-    #     jogador1 = game()
-    #     jogador2 = game()
-    #     jogador3 = game()
-    #     jogador4 = game()
+        print('Jogo')
 
-multiplayers()      # Chamando a função
+        nome1 = nomes_jogadores[0]
+        nome2 = nomes_jogadores[1]
+        nome3 = nomes_jogadores[2]
+        nome4 = nomes_jogadores[3]
+
+        lista1 = ['_'] * len(dici_vez_jogador[('id_jogador 1', nome1)])
+        lista2 = ['_'] * len(dici_vez_jogador[('id_jogador 2', nome2)])
+        lista3 = ['_'] * len(dici_vez_jogador[('id_jogador 3', nome3)])
+        lista4 = ['_'] * len(dici_vez_jogador[('id_jogador 4', nome4)])
+
+        chances1 = 6
+        chances2 = 6
+        chances3 = 6
+        chances4 = 6
+
+        tem_alguem_jogando = True
+        while tem_alguem_jogando:
+
+            nome1 = nomes_jogadores[0]
+            if ('id_jogador 1', nome1) in dici_vez_jogador:
+                print('Essa é a partida do jogador 1: Está jogando enquanto estiver o nome no dicionario')
+                erro = false
+                while not erro:
+                    (enforcou, acertou, chances1, erro) = game(dici_vez_jogador[('id_jogador 1', nome1)], lista1,
+                                                               chances1)
+
+                if enforcou == True
+                    print('E enforcou! Tente na próxima.\n\n')
+                    del dici_vez_jogador[('id_jogador 1', nome1)]
+
+                elif acertou == True
+                    print('Parabéns, você acertou todas as palavras!!',
+                          dici_vez_jogador[('id_jogador 1', nome1)].capitalize(), '\n\n')
+                    del dici_vez_jogador[('id_jogador 1', nome1)]
+
+        jogador1 = game()
+        jogador2 = game()
+        jogador3 = game()
+        jogador4 = game()
+
+    multiplayers()  # Chamando a função
